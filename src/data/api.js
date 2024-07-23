88c9c08afb384cc2f8c8b7600dce2681c449ddc0
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-07-23 04:46:40 +0200

    Add methods to fetch and display product details in modal using product ID

--- src/data/api.js
+++ src/data/api.js
@@ -2,6 +2,9 @@ import { data } from "autoprefixer";
 
 export default () => ({
   products: [],
+  selectedProduct: null,
+  modalOpen: false, 
+
   async getProducts() {
     try {
       let response = await fetch(`https://fakestoreapi.com/products`);
@@ -18,13 +21,31 @@ export default () => ({
     }
   },
 
-  showProductModal(product) {
-    this.selectedProduct = product;
+  async getProductDetails(productId) {
+    try {
+      let response = await fetch(
+        `https://fakestoreapi.com/products/${productId}`
+      );
+
+      if (!response.ok) {
+        throw new Error();
+      }
+
+      let data = await response.json();
+
+      this.selectedProduct = data;
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  },
+
+  async showProductModal(productId) {
+    await this.getProductDetails(productId); // Fetch product details by ID
     this.modalOpen = true;
   },
 
   closeModal() {
     this.modalOpen = false;
   },
-  
 });
+
