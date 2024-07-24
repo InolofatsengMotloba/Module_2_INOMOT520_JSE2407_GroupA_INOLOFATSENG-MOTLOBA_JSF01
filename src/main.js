bfabb5cbdfe5b9e6715aba70d9f5e68ce5130a14
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-07-24 08:54:37 +0200

    Add comments to main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
-import "./style.css";
-import Alpine from "alpinejs";
-import api from "./data/Alpine";
+import "./style.css"; // Import the main CSS file for styling
+import Alpine from "alpinejs"; // Import the Alpine.js library
+import alpine from "./data/Alpine"; // Import the module which handles API requests & functions for app
 
-window.Alpine = Alpine;
+window.Alpine = Alpine; // Make Alpine available globally on the window object
 
+// Listen for the Alpine.js initialization event
 document.addEventListener("alpine:init", () => {
-  Alpine.data("products", api)
+  // Register the 'products' component with Alpine.js, using the data from the module
+  Alpine.data("products", alpine);
 });
 
+// Start Alpine.js
 Alpine.start();
